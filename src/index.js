829d981386b290cf4daf82bb3b199b1c99418878
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-25 14:40:32 -0300

    projeto

--- src/index.js
+++ src/index.js
@@ -9,15 +9,14 @@ let valores={};
 const media= require('./media.js');
 
 app.post("/media", (req, res) => {
-
 const { nota1, nota2 } = req.body;
 const valores = { nota1, nota2 };
 const resultado={
-  nota1:
-  nota2:
-  Media:media(valores)
-}
-res.status(201).json(valores);
+  nota1 ,
+  nota2 ,
+  Media: media(valores)
+};
+res.status(201).json(resultado);
 });
 
 
