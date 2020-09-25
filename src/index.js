06f4e82209a09ab1c59237ad9113c60eec0dfc1a
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-25 14:21:36 -0300

    projeto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-var http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+app.use(cors());
+app.use(express.json());
+let valores={}
+
+const media= require('./media');
+
+app.post("/media", (req, res) => {
+
+const { nota1, nota2 } = req.body;
+const valores = { nota1, nota2 };
+valores.push(media(nota1,nota2));
+
+
+  res.status(201).json(valores);
+});
+
+
+app.get('media', (req, res) => {
+  const nota1 = Number(req.query.nota1);
+
+  const nota2 = Number(req.query.nota2);
+
+
+  
+
+
+app.listen(3000, () => {
+  console.log(`Media API is running!`);
+});
+
+
---
+++ src/media.js
@@ -0,0 +1,10 @@
+function media(nota1,nota2){
+  const media=(nota1+nota2)/2;
+
+if (media >=7)
+ alert("Media maior que 7"+media);
+ else
+ alert("Media menor que 7"+media);
+};
+
+module.exports =media;
