99336c3233fb300b73afaa6878b3924c5cd9baf7
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-28 12:48:42 -0300

    função backend

--- src/media.js
+++ src/media.js
@@ -2,9 +2,9 @@ function media(nota1, nota2) {
   const media = (nota1 + nota2) / 2;
 
   if (media >= 7) 
-  return (`Media maior que 7,= ${media}`);
+  return (`Media maior que 7. Resultado ${media}`);
   else 
-  return (`Media menor que 7,= ${media}`);
+  return (`Media menor que 7. Resultado ${media}`);
 };
 
 module.exports = media;
