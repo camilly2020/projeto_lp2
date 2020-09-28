7f4137ec454fbc5203a8cd1504d077fcc8ba1891
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-28 12:36:50 -0300

    projeto

--- src/media.js
+++ src/media.js
@@ -1,8 +1,10 @@
 function media(nota1, nota2) {
   const media = (nota1 + nota2) / 2;
 
-  if (media >= 7) return "Media maior que 7" + media;
-  else return "Media menor que 7" + media;
+  if (media >= 7) 
+  return ("Media maior que 7" + media);
+  else 
+  return ("Media menor que 7" + media);
 }
 
 module.exports = media;
