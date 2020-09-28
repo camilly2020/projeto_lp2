0e2ed5945e6d340d13a198770f5c7ab9e89e662d
camilly2020 <61083157+camilly2020@users.noreply.github.com>
2020-09-28 12:11:26 -0300

    projeto

--- src/index.js
+++ src/index.js
@@ -4,22 +4,20 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-let valores={};
+let valores = [];
 
-const media= require('./media.js');
+const media = require("./media.js");
 
 app.post("/media", (req, res) => {
-const { nota1, nota2 } = req.body;
-const valores = { nota1, nota2 };
-const resultado={
-  nota1 ,
-  nota2 ,
-  Media: media(valores)
-};
-res.status(201).json(resultado);
+  const { nota1, nota2 } = req.body;
+  const valores = { nota1, nota2 };
+  const resultado = {
+    nota1,
+    nota2,
+    Media: media(nota1, nota2)
+  };
+  res.status(201).json(resultado);
 });
-
-
 
 app.listen(3000, () => {
   console.log(`Media API is running!`);
